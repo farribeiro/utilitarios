b44bd97a9655575f122aba796396ab7878ff8bf0
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2010-06-03 18:11:33 -0300

    Melhorias

--- od.sh
+++ od.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ffmpeg -i "pgm"$1"_cv_integra.flv" -s 640x480 -r 23.975 -b 1024k -vcodec libxvid -ab 128k -ar 48000 -acodec libmp3lame od_$1.avi
+ffmpeg -i "pgm"$1"_cv_integra.flv" -r 23.975 -b 1024k -vcodec libxvid -ab 128k -ar 48000 -acodec libmp3lame od_$1.avi
