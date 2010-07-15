993e17b4bb564294312c21ef056f69bd1bb7ccef
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2010-07-14 23:19:53 -0300

    Acersentado melhorias no divx.py

--- divx.py
+++ divx.py
@@ -5,23 +5,29 @@ import os
 import sys
 
 class divx:
-    __res720 = "1280:720"
-    __res480 = "720:480"
+    hd720 = "1280:720"
+    sd480 = "720:480"
+    hd480 = "854:480"
 
-    def comando(self, res):
-        os.system("mencoder \"" + sys.argv[1] + "\" -oac mp3lame -lameopts cbr=128 -ovc lavc -lavcopts vcodec=msmpeg4:vbitrate=1800:aspect=16/9 -ofps 23.976 -vf scale=" + res + " -o \"" + sys.argv[1] + "_divx.avi\"")
+    def converte(self, res):
+        os.system("mencoder \"" + sys.argv[1] + "\" -oac mp3lame -lameopts cbr=128 -ovc " +
+                  "lavc -lavcopts vcodec=msmpeg4:vbitrate=1800:aspect=16/9 -ofps 23.976 -vf scale="
+                  + res + " -o \"" + sys.argv[1][:-4] + "_divx.avi\"")
     
     def __init__(self):
         print "Escolha a resolução"
-        print "1 => 480"
-        print "2 => 720"
+        print "1 => SD480 (720x480)"
+        print "2 => HD480 (854x480)"
+        print "3 => HD720"
         
-        fmt = raw_input("Formato:")
+        resolucao = raw_input("Opção: ")
         
-        if (fmt == "1"):
-            print self.comando(self.__res480)
-        elif (fmt == "2"):
-            self.comando(self.__res720)
+        if (resolucao == "1"):
+            self.converte(self.sd480)
+        elif (resolucao == "2"):
+            self.converte(self.hd480)
+        elif (resolucao == "3"):
+            self.converte(self.hd720)
         else:
-            print "Nao existe"                            
-d = divx()        
+            print "Opção inválida"                            
+d = divx()
