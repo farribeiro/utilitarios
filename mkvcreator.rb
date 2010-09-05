c435760de862c0c8f9ff36a9e7cd7e812fd3ff98
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2010-09-05 15:08:34 -0300

    Trocando nome de arquivo

--- mkvcreator.rb
+++ mkvcreator.rb
@@ -4,18 +4,23 @@
 
 #Este script faz a junção de legenda externa junto ao container MASTROSKA
 
-class Avi2mkv
+class Mkvcreator
 
   def initialize
     @vin = ARGV[0].to_s
-    @vout = ARGV[1].to_s
+    @vout = ARGV[1].chomp(".mkv").to_s
     #    puts "verdadeiro"  if @vin.
+    #    avi2mkv
     sub4mkv
   end
 
+  def avi2mkv
+    
+  end
+
   def sub4mkv
-    system "mkvmerge -o "+@vout.to_s+" "+@vin.to_s+" --language 0:por "+@vin.chomp(".mkv").to_s+ ".sub"
+    system "mkvmerge -o "+@vout.to_s+" "+@vin.to_s+" --language 0:por "+@vin.to_s+ ".sub"
   end
 end
 
-s = Avi2mkv.new
+s = Mkvcreator.new
