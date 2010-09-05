f6d26418f54af68f1625f657859749718cdcaef3
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2010-09-05 16:04:14 -0300

    Acresentando funcionalidade de conversão de conteiner

--- mkvcreator.rb
+++ mkvcreator.rb
@@ -2,21 +2,22 @@
 
 #Autor: Fábio Rodrigues Ribeiro
 
-#Este script faz a junção de legenda externa junto ao container MASTROSKA
+#Este script faz a conversão para container MASTROSKA e a 
+#junção de legenda externa no container MASTROSKA
 
 class Mkvcreator
 
   def initialize
+    # @codec = libx264
     @vin = ARGV[0].to_s
-    @vout = ARGV[1].chomp(".mkv").to_s
-    #    puts "verdadeiro"  if @vin.
-    #    avi2mkv
+    @vout = ARGV[1].chomp(".mkv").to_s    
+  #  avi2mkv
     sub4mkv
   end
 
-  def avi2mkv
-    
-  end
+#  def avi2mkv
+ #   system "mencoder "+ARGV[0].to_s+" -oac copy -ovc copy -o "+@vout.to_s+".mkv"
+ # end
 
   def sub4mkv
     system "mkvmerge -o "+@vout.to_s+" "+@vin.to_s+" --language 0:por "+@vin.to_s+ ".sub"
