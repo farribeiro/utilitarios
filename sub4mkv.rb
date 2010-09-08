06716d55cd7b1afe1b9a9f04472ea1c90fecfc41
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2010-09-07 21:55:45 -0300

    Repensado na utilidade do script

---
+++ sub4mkv.rb
@@ -0,0 +1,15 @@
+#!~/jruby/jruby
+
+#Autor: Fábio Rodrigues Ribeiro
+
+#Este script faz  junta o video e legenda externa no container MASTROSKA
+
+class Sub4mkv
+  def initialize
+    @vin = ARGV[0].to_s
+    @vout = ARGV[1].to_s
+    system "mkvmerge -o "+@vout.to_s+" "+@vin.to_s+" --language 0:por "+ @vin.chomp(".mkv").to_s+".sub"
+  end
+end
+
+s = Sub4mkv.new
