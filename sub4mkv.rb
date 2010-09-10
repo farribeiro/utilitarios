34ba49b6f21d5e509d70b19eb8ed5a5be6d0cd0a
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2010-09-10 18:31:57 -0300

    Retirando algumas redundancias

--- sub4mkv.rb
+++ sub4mkv.rb
@@ -6,8 +6,8 @@
 
 class Sub4mkv
   def initialize
-    @vin = ARGV[0].to_s
-    @vout = ARGV[1].to_s
+    @vin = ARGV[0]
+    @vout = ARGV[1]
     system "mkvmerge -o "+@vout.to_s+" "+@vin.to_s+" --language 0:por "+ @vin.chomp(".mkv").to_s+".sub"
   end
 end
