a9506a8eec2a2e84aa6515856b877b98a7bde753
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2010-09-14 00:32:46 -0300

    Acresentado informações no script

--- gcp.sh
+++ gcp.sh
@@ -2,4 +2,7 @@
 
 # Este script simplifica a ativação de parametros para o google chrome
 
-google-chrome --enable-accelerated-compositing #--enable-webgl
+# --enable-accelerated-compositing          habilita aceleração por video(dev)
+# --enable-match-preview                    habilita google instant via omnibox(dev)
+# --enable-webgl                            habilita recursos opengl html5(dev)
+google-chrome --enable-accelerated-compositing --enable-match-preview #--enable-webgl
