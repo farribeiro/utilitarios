4e2b4fce07b6eac713f97593a6240d143c932905
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2011-01-31 21:37:08 -0200

    Feito esqueleto para o utilitário de migração

--- migra.sh
+++ migra.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
+# Este script é muito semelhante aos services dos derivados do DEBIANs
+# É necessário ativar com atencedencia o tuncfg
 
-#dd if=? of=?
 
+clonar(){
+	d if=$2 of=imagem_$2
+}
 
-mkfs.??
-rsync -Cav ? ?
+migrar(){
+	rsync -Cav $2 $3 
+}
+
+case "$1" in
+	'clonar')
+		clonar
+	;;
+	'to-ext4')
+		mkfs.ext4 $3
+		migrar
+	;;
+	'to-raiserFS')
+		mkfs.raiserfs $3
+		migrar
+	;;
+esac
