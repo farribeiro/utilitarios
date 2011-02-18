786a38dff9f2c73a84ff1a5495fcbac99848242c
Fabio Rodrigues Ribeiro <fribeiro@scopus.com.br>
2011-02-18 14:04:07 -0200

    Feito melhorias no script

--- migra.sh
+++ migra.sh
@@ -20,4 +20,15 @@ case "$1" in
 		mkfs.raiserfs $3
 		migrar
 	;;
+	'forca-migracao')
+	    migrar
+       ;;
+       'help')
+            echo "Este utilitário realiza facilita a migração de de dados para outra HD/PARTIÇÃO para cópia utiliza o utilitário RSYNC e clonagem o DD
+
+Comandos aceitos:
+
+clonar    Clonagem da partição/hd
+to-ext4   Prepara a partição de destino para receber o FS ext4 e migar os dados"
+         ;;     
 esac
