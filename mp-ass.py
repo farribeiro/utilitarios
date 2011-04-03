ee58ecea2777d658d54ff5f5db7d142df9a615bc
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2011-04-03 00:49:00 -0300

    Acresentado licença

--- mp-ass.py
+++ mp-ass.py
@@ -4,17 +4,19 @@
 # Autor: Fábio Rodrigues Ribeiro
 
 # Este script ativa o plugin ASS para mplayer, muito utilizado em container 
-# Mastroska para legendas SRT
-# A condição de que a legenda tenha o mesmo nome do avi
+# Mastroska para legendas SRT, a condição de que a legenda tenha o mesmo nome do avi
 
 # Feita alteração no nome do arquivo do script para ser menor
+
+# Copying and distribution of this file, with or without modification, are permitted
+# in any medium without royalty provided the copyright notice and this notice are 
+# preserved.  This file is offered as-is, without any warranty.
 
 import os
 import sys
 
 
 class mplayerass:
-    
     def __init__(self):
         os.system("mplayer -utf8 -ass -ass-font-scale 2 " + sys.argv[1])
         
