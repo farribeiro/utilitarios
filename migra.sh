627724ae63732a1ab9924fd130522483a34d2ae0
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2011-06-19 14:09:44 -0300

    Adicionado rsync para o script

--- migra.sh
+++ migra.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+# (c) Fábio Rodrigues Ribeiro - http://farribeiro.blogspot.com
+
+# Copying and distribution of this file, with or without modification, are permitted
+# in any medium without royalty provided the copyright notice and this notice are 
+# preserved.  This file is offered as-is, without any warranty.
+
 clonar(){
-	d if=$2 of=imagem_$2
+	dd if=$2 of=imagem_$2
 }
 
 migrar(){
 	rsync -av $2 $3 
 }
 
+bk(){
+	rsync -av --delete $2 $3
+}
 case "$1" in
 	'clonar')
 		clonar
@@ -19,5 +28,8 @@ case "$1" in
 	'to-raiserFS')
 		mkfs.raiserfs $3
 		migrar
+	;;
+	'backup')
+		bk
 	;;
 esac
