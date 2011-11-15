649c6e47bc919cec4d414d42b6f6a1fbb35d44f0
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2011-11-15 13:12:07 -0200

    Acresentado linha para cópia em pendrive

--- migra.sh
+++ migra.sh
@@ -32,6 +32,9 @@ case "$1" in
 	'dd')
 		clonar
 	;;
+	'bk2mk')
+		rsync -av --delete --size-only --modify-window=1 $2 $3
+	;;
        'help')
             echo "Este utilitário facilita a migração de de dados para outra HD/PARTIÇÃO para cópia utilizando o utilitário RSYNC para e para clonagem usando DD
 
@@ -43,6 +46,7 @@ bk    Faz backup e apaga automaticamente arquivos que não existe mais na origem
 dd    Cria e recupera imagem da partição/hd
 se    Prepara a partição de destino com ext4 e migar os dados
 sr    Prepara a partição de destino com raiserFS e migar os dados
+bk2mk Faz backup para pendrive
 help  Imprime este guia"
          ;;     
 esac
