f9e30ed5c65cc8bcd301cf40c8da99bfc80becfc
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2011-12-11 20:32:43 -0200

    Colocado o Java nas variavéis de ambiente do xorg

---
+++ conf/xorgvars.sh
@@ -0,0 +1,13 @@
+# Este é o arquivo para variaveis de ambiente para o xorg, no terminal no X as variaveis estão carregadas. Somente colocar uma referencia a este arquivo no profile
+
+# No ubuntu colocar em /etc/profile.d/
+
+# (c) Fábio Rodrigues Ribeiro - http://farribeiro.blogspot.com
+
+# Copying and distribution of this file, with or without modification, are permitted
+# in any medium without royalty provided the copyright notice and this notice are
+# preserved.  This file is offered as-is, without any warranty.
+
+export JAVA_HOME=/home/fabio/jdk
+export CLASSPATH=.
+export PATH=$PATH:$JAVA_HOME:$JAVA_HOME/bin:$CLASSPATH
