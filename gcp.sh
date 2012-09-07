b20adbd6b85f3c9a990847b5addf0aae834c34b8
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2012-09-07 16:43:32 -0300

    Redundante

--- gcp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Este script simplifica a ativação de parametros para o google chrome
-
-# --enable-accelerated-compositing          habilita aceleração por video(dev)
-# --enable-match-preview                    habilita google instant via omnibox(dev)
-# --enable-webgl                            habilita recursos opengl html5(dev)
-
-google-chrome --enable-accelerated-compositing --enable-match-preview &#--enable-webgl
