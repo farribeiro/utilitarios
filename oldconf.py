112f3b5cbcf7ca852095a2b6224bb653aae799db
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2013-03-11 22:36:14 -0300

    Limpeza do homedir

---
+++ oldconf.py
@@ -0,0 +1,15 @@
+#!/bin/python
+
+# Script para fazer a limpeza do .home, onde o usuário seleciona em multipla escolha os diretórios para limpeza!
+
+import os, sys
+
+class oconf:
+	__homedir = "~"
+
+#	def search():
+
+
+	def __init__(self):
+		print "Home dir:", self.__homedir
+o = oconf()
