f55d6f5d3914da39815d9acd48f56e0ca8e892f3
Fábio Rodrigues Ribeiro <farribeiro@yahoo.com.br>
2013-03-18 15:20:17 -0300

    Acresentado novos utiliarios

--- finter.ps1
+++ finter.ps1
@@ -1,1 +1,11 @@
 systeminfo | findstr instalação
+netsh reset winsock
+ipconfig /release
+ipconfig /renew
+arp -d *
+nbtstat -R
+nbstat -RR
+ipconfig /flushdns
+ipconfig /registerdns
+netsh interface ip delete arpcache
+pause
