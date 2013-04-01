9928897f0bf4d5001f5ef820f1fed476e3762f94
Fábio Rodrigues Ribeiro <farribeiro@yahoo.com.br>
2013-04-01 10:31:01 -0300

    Melhorias nas regex

--- toner_xerox.sh
+++ toner_xerox.sh
@@ -12,23 +12,21 @@ limpa
 
 /usr/lib/nagios/plugins/check_snmp_printer $1 public CONSUM ALL > $bruto
 
-echo "<table border=\"1\">
-<tr><th>Printer</th><th>BLACK</th><th>MAGENTA</th><th>YELLOW</th><th>CYAN</th></tr>" > $limpo
+echo "<table border=\"1\">" > $limpo
+# <tr><th>Printer</th><th>BLACK</th><th>YELLOW</th><th>MAGENTA</th><th>CYAN</th></tr>" > $limpo
 
-sed "s/, \?/\n/g" $bruto |
-sed "s/;;\?/\n/g" |
-#sed "s/.*Developer.*|OK|WARNING|^.*=[0-9]\+$|[0-9]\+$//g" |
-sed "s/^.*Developer.*$//g" |
-sed "s/^.*=.*$//g" |
-sed "s/OK//g" |
-sed "s/WARNING//g" |
-sed "s/[0-9]\+$//g" |
-sed "s/^\(.*\]\).*\([0-9]\{2,3\}%\).*$/<TR><TD>\1<\/TD><TD>\2<\/TD><\/TR>/g" |
-sed "s/^\(.*\) is.*\([0-9]\{3,3\}%\).*$/<TR><TD>\1<\/TD><TD>\2<\/TD><\/TR>/g" |
-sed "s/<TR>\(.*Magenta.*<\/TR>\)/<TR STYLE=\"background-color: MAGENTA\">\1/g" |
-sed "s/<TR>\(.*Yellow.*<\/TR>\)/<TR STYLE=\"background-color: YELLOW\">\1/g" |
-sed "s/<TR>\(.*Cyan.*<\/TR>\)/<TR STYLE=\"background-color: CYAN\">\1/g" |
-sed "/^$/d" >> $limpo
+sed -r "s/, ?/\n/g" $bruto |
+sed -r "s/;;?/\n/g" |
+sed -r "s/^(.+Developer|.+=).*$//g" |
+sed -r "s/OK//" |
+sed -r "s/WARNING//g" |
+sed -r "s/[0-9]+$//g" |
+sed -r "s/^(.+\]).+([0-9]{2,}%).*$/<TR><TD>\1<\/TD><TD>\2<\/TD><\/TR>/g" |
+sed -r "s/^(.+) is.+([0-9]{3,}%).*$/<TR><TD>\1<\/TD><TD>\2<\/TD><\/TR>/g" |
+sed -r "s/<TR>(.*Magenta.*<\/TR>)/<TR STYLE=\"background-color: MAGENTA\">\1/g" |
+sed -r "s/<TR>(.*Yellow.*<\/TR>)/<TR STYLE=\"background-color: YELLOW\">\1/g" |
+sed -r "s/<TR>(.*Cyan.*<\/TR>)/<TR STYLE=\"background-color: CYAN\">\1/g" |
+sed -r "/^$/d" >> $limpo
 
 echo "</table>" >> $limpo
 
