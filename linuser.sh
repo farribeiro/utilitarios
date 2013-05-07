d448687f765522a5b672d67e419d039de3593fb5
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2013-05-07 20:38:55 -0300

    Acresentado menu

--- linuser.sh
+++ linuser.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
 for a in $(cat /etc/passwd | cut -d ":" -f 1); do groups $a ; done
+
+while : ; do
+
+op = $(
+ dialog --stdout \
+	--menu 'Escolha um opcao: ' \
+	0 0 0 \
+	1 'Adicionar usuário' \
+	2 'Listar usuários '  \
+	3 'Deletar usuário' \
+	0 'Sair' \
+)
+[ $? -ne 0 ] && break
+
+done
