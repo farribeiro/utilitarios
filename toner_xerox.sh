5a9c92a0c406922efa40014fae148c621fb00e31
Fábio Rodrigues Ribeiro <farribeiro@yahoo.com.br>
2013-05-07 16:50:47 -0300

    Melhorado script

--- toner_xerox.sh
+++ toner_xerox.sh
@@ -16,8 +16,10 @@ echo "<table border=\"1\">" > $limpo
 # <tr><th>Printer</th><th>BLACK</th><th>YELLOW</th><th>MAGENTA</th><th>CYAN</th></tr>" > $limpo
 
 sed -r "s/, ?|;;?/\n/g" $bruto |
-sed -r "s/^(.+Developer|.+=).*$|OK|WARNING|[0-9]+$//g" |
-sed -r "s/^(.+\]?).i?s?.+([0-9]{2,}%).*$/<TR><TD>\1<\/TD><TD>\2<\/TD><\/TR>/g" |
+sed -r "s/^.*Developer.*$//g" |
+sed -r "s/^.+=.*$//g" |
+sed -r "s/OK|WARNING|[0-9]+$//g" |
+sed -r "s/^(.+\]?).+(is)?.+([0-9]{2}%).*$/<TR><TD>\1<\/TD><TD>\3<\/TD><\/TR>/g" |
 #sed -r "s/^(.+) is.+([0-9]{3,}%).*$/<TR><TD>\1<\/TD><TD>\2<\/TD><\/TR>/g" |
 sed -r "s/<TR>(.*Magenta.*<\/TR>)/<TR STYLE=\"background-color: MAGENTA\">\1/g" |
 sed -r "s/<TR>(.*Yellow.*<\/TR>)/<TR STYLE=\"background-color: YELLOW\">\1/g" |
@@ -26,7 +28,7 @@ sed -r "/^$/d" >> $limpo
 
 echo "</table>" >> $limpo
 
-cat $limpo #| mutt -e "set content_type=text/html" -s "Consumíveis $1" $2
+cat $limpo # mutt -e "set content_type=text/html" -s "Consumíveis $1" $2
 
 limpa
 
