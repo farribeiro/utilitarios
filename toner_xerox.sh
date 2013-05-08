16501a3ace419eaafa1d258d4e6e42c033e6175a
Fabio Rodrigues Ribeiro <farribeiro@yahoo.com.br>
2013-05-08 16:28:44 -0300

    Corrigido e aprimorado para lexmark

--- toner_xerox.sh
+++ toner_xerox.sh
@@ -18,9 +18,8 @@ echo "<table border=\"1\">" > $limpo
 sed -r "s/, ?|;;?/\n/g" $bruto |
 sed -r "s/^.*Developer.*$//g" |
 sed -r "s/^.+=.*$//g" |
-sed -r "s/OK|WARNING|[0-9]+$//g" |
-sed -r "s/^(.+\]?).+(is)?.+([0-9]{1,3}?%).*$/<TR><TD>\1<\/TD><TD>\3<\/TD><\/TR>/g" |
-#sed -r "s/^(.+) is.+([0-9]{3,}%).*$/<TR><TD>\1<\/TD><TD>\2<\/TD><\/TR>/g" |
+sed -r "s/OK|WARNING|CRITICAL|^[0-9]+$|^ ?[0-9].*$//g" |
+sed -r "s/^(.+\]?).+is at ([0-9]{1,3}?%).*$/<TR><TD>\1<\/TD><TD>\2<\/TD><\/TR>/g" |
 sed -r "s/<TR>(.*Magenta.*<\/TR>)/<TR STYLE=\"background-color: MAGENTA\">\1/g" |
 sed -r "s/<TR>(.*Yellow.*<\/TR>)/<TR STYLE=\"background-color: YELLOW\">\1/g" |
 sed -r "s/<TR>(.*Cyan.*<\/TR>)/<TR STYLE=\"background-color: CYAN\">\1/g" |
@@ -28,7 +27,7 @@ sed -r "/^$/d" >> $limpo
 
 echo "</table>" >> $limpo
 
-cat $limpo # mutt -e "set content_type=text/html" -s "Consumíveis $1" $2
+cat $limpo | mutt -e "set content_type=text/html" -s "Consumíveis $1" $2
 
 limpa
 
