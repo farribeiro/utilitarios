57d68556a8d60750b38be99cc2d14f3d5367797d
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2014-07-24 11:23:46 -0300

    Arquivo basico de configuração do puppet

---
+++ conf/site.pp
@@ -0,0 +1,26 @@
+node 	"common" {
+	package {[
+		"htop",
+		"man",
+		"tmux",
+		"heartbeat",
+		"sudo",
+		"puppet",
+		"ntp",
+		"ntpdate",
+		"nss-pam-ldapd",
+		"krb5-workstation",
+		"pam_krb5",
+		"nscd",
+		"openldap-clients",
+		"mutt",
+		"net-snmp",
+		"rsync",
+		"at"
+		]:
+		ensure => present,
+	}
+}
+
+node	"puppetagent.localdomain",
+	"nagios-vp.bradw01.local" inherits "common" {}
