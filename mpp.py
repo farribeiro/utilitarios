3f4041d9f0ae4105052ece3595a41a76fa3d894c
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2015-09-13 12:37:34 -0300

    Acertado identação, espaços em branco e comentários

--- mpp.py
+++ mpp.py
@@ -29,17 +29,17 @@ class mpplus:
 
 	def converte(self, res):
 		os.system("mencoder \"" + sys.argv[2] + "\" -oac mp3lame -lameopts cbr=128 -ovc " +
-				  "lavc -lavcopts vcodec=msmpeg4:vbitrate=1800:aspect=16/9 -ofps 23.976 -vf scale="
-				  + res + " -o \"" + sys.argv[2][:-4] + "_divx.avi\"")
+				"lavc -lavcopts vcodec=msmpeg4:vbitrate=1800:aspect=16/9 -ofps 23.976 -vf scale="
+				+ res + " -o \"" + sys.argv[2][:-4] + "_divx.avi\"")
 	
-	def divx(self): 
+	def divx(self):
 		print "Escolha a resolução"
 		print "1 => SD480 (720x480)"
 		print "2 => HD480 (854x480)"
 		print "3 => HD720 (1280x720)"
-		
+
 		resolucao = raw_input("Opção: ")
-		
+
 		if (resolucao == "1"):
 			self.converte(self.sd480)
 		elif (resolucao == "2"):
@@ -47,12 +47,12 @@ class mpplus:
 		elif (resolucao == "3"):
 			self.converte(self.hd720)
 		else:
-			print "Opção inválida"							
-			
+			print "Opção inválida"
+
 	def ass(self):
 		os.system("mplayer -utf8 -ass -ass-font-scale 2 " + sys.argv[2])
 
 	def dvdnav(self):
 		os.system("mplayer -mouse-movements dvdnav://")
-    	
+
 m = mpplus()
