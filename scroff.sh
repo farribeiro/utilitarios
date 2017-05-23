9c40241eec1b09b99245147a57e8cce2e68781fd
Fabio Rodrigues Ribeiro <farribeiro@gmail.com>
2017-05-23 17:32:31 -0300

    Quebra de linhas

--- scroff.sh
+++ scroff.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # (c) Fábio Rodrigues Ribeiro - http://farribeiro.blogspot.com
 
-# Copying and distribution of this file, with or without modification, are permitted
-# in any medium without royalty provided the copyright notice and this notice are
-# preserved.  This file is offered as-is, without any warranty.
+# Copying and distribution of this file, with or without modification, are
+# permitted in any medium without royalty provided the copyright notice and this
+# notice are preserved.  This file is offered as-is, without any warranty.
 
-# Este script faz o desligamento da tela, para restaurar fazer qualquer atividade no computador
+# Este script faz o desligamento da tela, para restaurar fazer qualquer
+# atividade no computador
 
 xset -display :0.0 dpms force off
